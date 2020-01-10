7c5e50e4642000733e507a813d072b02ed80ab14
Dean Clarke <clarkedean25@gmail.com>
2020-01-10 12:13:15 +1100

    Update status-callback.js

--- Functions/status-callback.js
+++ Functions/status-callback.js
@@ -1,82 +1,81 @@
 //Fetch parent call leg
 function fetchParentCall(client, parentCallSid) {
-return client.calls(parentCallSid)
-		.fetch()
+    return client.calls(parentCallSid)
+        .fetch()
 }
-
+​
 //Fetch items from sync map
 function fetchSyncItem(client, context) {
-return client.sync.services(context.SYNC_SERVICE_SID)
-		.syncMaps('ActiveCall')
-		.syncMapItems
-		.list()
+    return client.sync.services(context.SYNC_SERVICE_SID)
+        .syncMaps('ActiveCall')
+        .syncMapItems
+        .list()
 }
-
+​
 exports.handler = function (context, event, callback) {
-const client = context.getTwilioClient();
-
-fetchParentCall(client, event.ParentCallSid)
-		.then(call => {
-				console.log("hello")
-				fetchSyncItem(client, context)
-						.then(result => {
-								/**
-								 * If the result isn't equal to 0 update the sync map, else create the item 
-								 */
-								if (result.length !== 0) {
-										client.sync.services(context.SYNC_SERVICE_SID)
-												.syncMaps('ActiveCall')
-												.syncMapItems('Call')
-												.update({
-														data: {
-																ParentCall: {
-																		sid: call.sid,
-																		status: call.status
-																},
-																ChildCall: {
-																		sid: event.CallSid,
-																		status: event.CallStatus
-																}
-														}
-												})
-												.then(result => {
-														callback(null, result)
-												})
-												.catch(err => {
-														console.log(err)
-														callback(null, err)
-												})
-								} else {
-										client.sync.services(context.SYNC_SERVICE_SID)
-												.syncMaps('ActiveCall')
-												.syncMapItems
-												.create({
-														key: 'Call',
-														data: {
-																ParentCall: {
-																		sid: call.sid,
-																		status: call.status
-																},
-																ChildCall: {
-																		sid: event.CallSid,
-																		status: event.CallStatus
-																}
-														}
-
-												})
-												.then(result => {
-														console.log(result)
-														callback(null, result)
-												})
-												.catch(err => {
-														console.log(err)
-														callback(null, err)
-												});
-								}
-						});
-		})
-		.catch(err => {
-				console.log(err)
-				callback(null, err)
-		})
+    const client = context.getTwilioClient();
+​
+    fetchParentCall(client, event.ParentCallSid)
+        .then(call => {
+            fetchSyncItem(client, context)
+                .then(result => {
+                    /**
+                     * If the result isn't equal to 0 update the sync map, else create the item.
+                     */
+                    if (result.length !== 0) {
+                        client.sync.services(context.SYNC_SERVICE_SID)
+                            .syncMaps('ActiveCall')
+                            .syncMapItems('Call')
+                            .update({
+                                data: {
+                                    ParentCall: {
+                                        sid: call.sid,
+                                        status: call.status
+                                    },
+                                    ChildCall: {
+                                        sid: event.CallSid,
+                                        status: event.CallStatus
+                                    }
+                                }
+                            })
+                            .then(result => {
+                                callback(null, result)
+                            })
+                            .catch(err => {
+                                console.log(err)
+                                callback(null, err)
+                            })
+                    } else {
+                        client.sync.services(context.SYNC_SERVICE_SID)
+                            .syncMaps('ActiveCall')
+                            .syncMapItems
+                            .create({
+                                key: 'Call',
+                                data: {
+                                    ParentCall: {
+                                        sid: call.sid,
+                                        status: call.status
+                                    },
+                                    ChildCall: {
+                                        sid: event.CallSid,
+                                        status: event.CallStatus
+                                    }
+                                }
+​
+                            })
+                            .then(result => {
+                                console.log(result)
+                                callback(null, result)
+                            })
+                            .catch(err => {
+                                console.log(err)
+                                callback(null, err)
+                            });
+                    }
+                });
+        })
+        .catch(err => {
+            console.log(err)
+            callback(null, err)
+        })
 };
